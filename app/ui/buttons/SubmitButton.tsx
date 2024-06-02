1fb6de7107bbb7e90a5a706d7e0476cf930d6af0
SHRony <shrony101@gmail.com>
2024-06-03 03:01:54 +0600

    style: change form UI

--- app/ui/buttons/SubmitButton.tsx
+++ app/ui/buttons/SubmitButton.tsx
@@ -4,6 +4,6 @@ import { Button, IconButton } from "@mui/material";
 import Link from "next/link";
 export default function Sub({children, clickHandler} : Readonly<{children:ReactNode|string, clickHandler : () => void}>){
   return (
-    <Button onClick={clickHandler} style={{fontWeight : 'bold',background:'var(--primary)'}} variant="contained" className='flex flex-row justify-evenly items-center py-1 px-2 text-white'>{children}</Button>
+    <Button onClick={clickHandler} style={{fontWeight : 'bold',background:'var(--primary)'}} variant="contained" className='flex flex-row justify-evenly items-center py-1 px-2 text-white w-full'>{children}</Button>
   );
 }
