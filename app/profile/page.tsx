d79e7bbb05674d40b2eb26df5707a2966e691788
SHRony <shrony101@gmail.com>
2024-06-05 01:45:41 +0600

    fix: use useSearchParams inside suspense boundery

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, useContext, use } from 'react';
+import React, { useState, useEffect, useContext, Suspense } from 'react';
 import { authContext } from "@/app/lib/AuthProvider";
 import { redirect } from 'next/navigation';
 import { useSearchParams } from 'next/navigation'
@@ -14,15 +14,19 @@ import RatingLineChart from '../ui/cfviz/RatingLineChart';
 import ScatterChart from '../ui/cfviz/ScatterChart';
 import UserCard from '../ui/cards/UserCard';
 import UserCardSkeleton from '../ui/cards/UserCardSkeleton';
-export default function Page() {
+
+type UserInfoComponentProps = {
+  name: string | undefined;
+};
+
+const UserInfoComponent: React.FC<UserInfoComponentProps> = ({ name }) => {
   const auth = useContext(authContext);
   const [user, setUser] = useState<userType|null>(null);
   const [cfUser, setCfUser] = useState<cfUserType|null>(null);
-  const searchParams = useSearchParams();
+  
   useEffect(() => {
-    let username:string|undefined|null = searchParams.get('username');
-    if(!username && auth?.signedIn) username = auth?.user?.userName;
-    // if(auth && !(auth.signedIn) && !username) redirect('/login');
+    let username:string|null = name ?? null;
+    if(!username && auth?.signedIn) username = auth?.user?.userName??null;
     if(!username) return ;
     axios.get(`/api/userinfo?name=${username}`).then((res) => {
       if(res.data){
@@ -32,64 +36,74 @@ export default function Page() {
     }).catch((res)=>{
 
     });
-  }, [searchParams, auth]);
+  }, [name, auth]);
 
   return (
-  <div className="flex flex-col items-center w-full pt-20 gap-20">
-    <div className="flex flex-row flex-wrap w-full justify-center items-stretch gap-20">
-      {/* basic userinfo here */}
+    <div className="flex flex-col items-center w-full pt-20 gap-20">
+      <div className="flex flex-row flex-wrap w-full justify-center items-stretch gap-20">
+        {/* basic userinfo here */}
+        {
+          user != null ? (
+            <UserCard phone={''} password='' userName={user.userName} fullName={user.fullName} email={user.email} vjudgeHandle={user.vjudgeHandle} cfHandles={user.cfHandles} registrationNumber={user.registrationNumber}></UserCard>
+          ):(
+            <UserCardSkeleton></UserCardSkeleton>
+          )
+        }
+        {
+          cfUser != null ? (
+            <UserInfo maxRating={cfUser.maxRating} maxRank={cfUser.maxRank} lastActive={cfUser.lastActive} registered={cfUser.registered} contribution={cfUser.contribution} avatar={cfUser.avatar} name={cfUser.name}></UserInfo>
+          ) : (
+            <></>
+          )
+        }
+      </div>
       {
-        user != null ? (
-          <UserCard phone={''} password='' userName={user.userName} fullName={user.fullName} email={user.email} vjudgeHandle={user.vjudgeHandle} cfHandles={user.cfHandles} registrationNumber={user.registrationNumber}></UserCard>
-        ):(
-          <UserCardSkeleton></UserCardSkeleton>
+        cfUser != null ? (
+          <RatingLineChart lineData={cfUser.ratingChanges}></RatingLineChart>
+        ) : (
+          <></>
         )
       }
       {
         cfUser != null ? (
-          <UserInfo maxRating={cfUser.maxRating} maxRank={cfUser.maxRank} lastActive={cfUser.lastActive} registered={cfUser.registered} contribution={cfUser.contribution} avatar={cfUser.avatar} name={cfUser.name}></UserInfo>
+          <DifficultyBarChart barData={cfUser.diffData}></DifficultyBarChart>
+        ) : (
+          <></>
+        )
+      }
+      {
+        cfUser != null ? (
+          <CatagoryBarChart barData={cfUser.catData}></CatagoryBarChart>
+        ) : (
+          <></>
+        )
+      }
+  
+      {
+        cfUser != null ? (
+          <ScatterChart user={cfUser}></ScatterChart>
+        ) : (
+          <></>
+        )
+      }
+      {
+        cfUser != null ? (
+          <CalenderHeatmap user={cfUser}></CalenderHeatmap>
         ) : (
           <></>
         )
       }
     </div>
-    {
-      cfUser != null ? (
-        <RatingLineChart lineData={cfUser.ratingChanges}></RatingLineChart>
-      ) : (
-        <></>
-      )
-    }
-    {
-      cfUser != null ? (
-        <DifficultyBarChart barData={cfUser.diffData}></DifficultyBarChart>
-      ) : (
-        <></>
-      )
-    }
-    {
-      cfUser != null ? (
-        <CatagoryBarChart barData={cfUser.catData}></CatagoryBarChart>
-      ) : (
-        <></>
-      )
-    }
+  );
+}
 
-    {
-      cfUser != null ? (
-        <ScatterChart user={cfUser}></ScatterChart>
-      ) : (
-        <></>
-      )
-    }
-    {
-      cfUser != null ? (
-        <CalenderHeatmap user={cfUser}></CalenderHeatmap>
-      ) : (
-        <></>
-      )
-    }
-    
+export default function Page() {
+  const searchParams = useSearchParams();
+  const username = searchParams.get('username');
 
-  </div>)
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <UserInfoComponent username={username} />
+    </Suspense>
+  )
 }
