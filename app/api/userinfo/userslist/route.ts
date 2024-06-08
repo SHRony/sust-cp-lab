06abbf39206a71e2ebae002da7fcf225729d3f65
SHRony <shrony101@gmail.com>
2024-06-08 15:49:58 +0600

    fix: use join in userlist

--- app/api/userinfo/userslist/route.ts
+++ app/api/userinfo/userslist/route.ts
@@ -4,10 +4,17 @@ import dbTables from '@/app/lib/dbTables';
 import client from '@/app/api/dbclient';
 export async function GET(request:NextRequest) {
   try{
-    const response = await client.query(`
-      SELECT username, info FROM ${dbTables.cf_cache}
-    `);
-    return NextResponse.json({users: response.rows});
+    // write a code to join cf_cache and users table where SELECT ${dbTables.cf_cache}.username, ${dbTables.cf_cache}.info FROM ${dbTables.cf_cache} INNER JOIN ${dbTables.users} ON ${dbTables.cf_cache}.username = ${dbTables.users}.usernameSELECT ${dbTables.cf_cache}.username, ${dbTables.cf_cache}.info FROM ${dbTables.cf_cache} INNER JOIN ${dbTables.users} ON ${dbTables.cf_cache}.username = ${dbTables.users}.username matches and then return the username and info, if info doesn't exist return null as info
+    const res = await client.query(
+      `SELECT ${dbTables.users}.username, ${dbTables.cf_cache}.info FROM ${dbTables.cf_cache} FULL OUTER JOIN ${dbTables.users} ON ${dbTables.cf_cache}.username = ${dbTables.users}.username`
+    );
+    const users = res.rows.map(row => {
+      return {
+        username: row.username,
+        info: row.info ? row.info : null
+      }
+    });
+    return NextResponse.json({users: users});
   }catch{
     return NextResponse.json({ error: 'An error occurred' }, { status: 500 });
   }
