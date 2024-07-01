c480b3e91cbbe33d245ca312b9f37d4031305397
SHRony <shrony101@gmail.com>
2024-07-01 13:16:52 +0600

    feat: add compare users feature to the team creation dashboard

---
+++ app/api/contests/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import dbclient from "@/app/api/dbclient";
+import dbTables from "@/app/lib/dbTables";
+
+export async function GET(request: NextRequest) {
+  try {
+    const id = request.nextUrl.pathname.split('/').pop();
+    if(!id || id == '') return NextResponse.json({ error: 'Missing parameters' }, { status: 500 });
+    
+    const response = await dbclient.query(`
+      SELECT * FROM ${dbTables.contests} WHERE id = $1
+    `, [id]);
+    if(response.rowCount == 0) return NextResponse.json({ error: 'Contest not found' }, { status: 404 });
+    const contest = response.rows[0];
+    return NextResponse.json(contest);
+  } catch (error) {
+    console.error("Error querying database:", error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
+
